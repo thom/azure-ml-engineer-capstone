7afb15219a8653c68597dcb804751e23cdf5116f
Thomas Weibel <thom@ikhono.net>
2021-05-11 18:46:16 +0200

    Implement training script for HyperDrive experiment

--- training/train.py
+++ training/train.py
@@ -19,7 +19,7 @@ ds = TabularDatasetFactory.from_delimited_files(path=path)
 def clean_data(data):
     '''
     Clean the data
-    
+
     Parameters:
         data (TabularDataset): Dataset to be cleaned
 
@@ -27,7 +27,12 @@ def clean_data(data):
         x_df(pandas.DataFrame): Feature dataset
         y_df(pandas.DataFrame): Label dataset
     '''
-    pass
+    # Clean and one hot encode data
+    x_df = data.to_pandas_dataframe().dropna()
+
+    y_df = x_df.pop("DEATH_EVENT")
+
+    return x_df, y_df
 
 
 def main():
@@ -41,7 +46,31 @@ def main():
 
     run = Run.get_context()
 
-    pass
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--C',
+        type=float,
+        default=1.0,
+        help="Inverse of regularization strength. Smaller values cause stronger regularization")
+
+    parser.add_argument(
+        '--max_iter',
+        type=int,
+        default=100,
+        help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run.log("Regularization strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    model = LogisticRegression(
+        C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
 
 
 if __name__ == '__main__':
